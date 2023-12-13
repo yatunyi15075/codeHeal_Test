94f7729cf5c74f024f07f9c09c945fba05637b07
brian <yatunyi.15075@students.kyu.ac.ke>
2023-12-13 19:37:05 +0300

    adding files

---
+++ lib/widgets/navbar_roots.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:code/screens/home.dart';
+import 'package:code/screens/schedule_screen.dart';
+import 'package:code/screens/settings_screen.dart';
+import 'package:code/screens/messages_screen.dart';
+
+class NavBarRoots extends StatefulWidget {
+  const NavBarRoots({super.key});
+
+  @override
+  State<NavBarRoots> createState() => _NavBarRootsState();
+}
+
+class _NavBarRootsState extends State<NavBarRoots> {
+  int _selectedIndex = 0;
+  final _screens = [
+    // //Home screen
+    // HomeScreen(),
+    // //msg screen
+    // MessagesScreen(),
+    // //schedule screen
+    // ScheduleScreen(),
+    // //settings screen
+    // SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: Container(
+        height: 80,
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Color(0xFF7165D6),
+          unselectedItemColor: Colors.black26,
+          selectedLabelStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.chat_bubble_text_fill),
+              label: "Messages",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: "Schedule",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
